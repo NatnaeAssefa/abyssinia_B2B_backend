bbade037672d594a9b7f04f36b89185439a93e62
Natnael Assefa <114475991+NatnaeAssefa@users.noreply.github.com>
2026-05-04 10:39:12 +0300

    Refactor logger configuration for better clarity

--- src/utilities/logger/logger.ts
+++ src/utilities/logger/logger.ts
@@ -1,20 +1,25 @@
 import winston from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
 import { env } from "../../config";
 
-const loggerTransports = [];
-if (!env.PRODUCTION || true) {
-  loggerTransports.push(new winston.transports.Console());
+const transports = [
+  new winston.transports.Console(),
+];
+
+// Only use file logging in NON-production
+if (!env.PRODUCTION) {
+  const DailyRotateFile = require("winston-daily-rotate-file");
+
+  transports.push(
+    new DailyRotateFile({
+      filename: "xlogs/application-%DATE%.log",
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: true,
+      maxSize: "20m",
+      maxFiles: "30d",
+    })
+  );
 }
-loggerTransports.push(
-  new DailyRotateFile({
-    filename: `${env.PRODUCTION ? "./" : "x"}logs/application-%DATE%.log`,
-    datePattern: "YYYY-MM-DD",
-    zippedArchive: true,
-    maxSize: "20m",
-    maxFiles: "30d",
-  })
-);
+
 export const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
@@ -23,7 +28,7 @@ export const logger = winston.createLogger({
       return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
     })
   ),
-  transports: loggerTransports,
+  transports,
 });
 
 export default { logger };
